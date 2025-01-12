7014861df9c4667563fad44de16ebf5b68aa3cd1
yoonbada97 <yoonbada97@gmail.com>
2025-01-12 14:20:40 +0900

    delete TestController and add allow UserPath

--- src/main/java/com/greenart/security/controller/UserController.java
+++ src/main/java/com/greenart/security/controller/UserController.java
@@ -1,8 +1,19 @@
 package com.greenart.security.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class UserController {
 
+    @GetMapping("/user")
+    public ResponseEntity<Object> getUser() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("user");
+    }
 }
